eed26ea25f8191b03c1d288e79d95b44da5c0c0c
lizziegitau <lizziegitau0505@gmail.com>
2023-06-30 19:01:51 +0300

    Announcer component

--- tic-tac-toe-game/src/components/Announcer.jsx
+++ tic-tac-toe-game/src/components/Announcer.jsx
@@ -1,20 +1,48 @@
-import React from "react"
+import React from "react";
 
-export default function Announcer(){  
-  const announce = (type) => {
-    switch(type){
-        case PLAYERO_WON:
-            return 'Player <span class="playerO">O</span> Won 🏁';
-            break;
-        case PLAYERX_WON:
-            return  'Player <span class="playerX">X</span> Won 🏁';
-            break;
-        case TIE:
-            return 'Tie 🙂';
-    }
-    announcer.classList.remove('hide');
-}
+const Announcer = ({ winner, currentPlayer, gameOver, handleReset }) => {
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+      ];
+
+      const checkForWinner = () => {
+        for (let combination of winningCombinations) {
+          const [a, b, c] = combination;
+          if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+          ) {
+            return squares[a];
+          }
+        }
+        return null;
+      };
+  if (gameOver) {
+    if (winner) {
       return (
-        <section id="announcer"></section>
-      )
-}
+        <div className="announcer">
+          <h2>{`Player ${winner} Wins!`}</h2>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="announcer">
+          <h2>It's a draw!</h2>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className="announcer">
+      <h2>{`Player ${currentPlayer}'s Turn`}</h2>
+    </div>
+  );
+};
+
+export default Announcer;
