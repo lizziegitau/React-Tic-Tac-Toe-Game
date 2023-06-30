05016532f51d5e848226c98a8e635ed788b9f9f0
lizziegitau <lizziegitau0505@gmail.com>
2023-06-30 22:29:02 +0300

    game changes

--- tic-tac-toe-game/src/components/PlayerDisplay.jsx
+++ tic-tac-toe-game/src/components/PlayerDisplay.jsx
@@ -1,13 +1,16 @@
 import React from "react"
+import Announcer from "./Announcer";
 
 export default function PlayerDisplay() {
-    const [turn,setTurn] = React.useState("X");
-    const [cells,setCells] = React.useState(Array(9).fill(''));
+    const [turn,setTurn] = React.useState("X")
+    const [cells,setCells] = React.useState(Array(9).fill(''))
+
     function handleTileClick(num){
         if (cells[num] !== ''){
             alert("already clicked");
             return;
         }
+
         let squares = [...cells];
         if (turn === "X"){
             squares[num]= "X";
@@ -21,12 +24,17 @@ export default function PlayerDisplay() {
      const Square = ({num}) => {
         return <button className="tile" onClick={() =>handleTileClick(num)}>{cells[num]}</button>;
     };
+
+    function resetBoard () {
+        setTurn("X")
+        setCells(Array(9).fill(""))
+    }
     
     return (
         <div className="player-display">
             <h2 className="current-player">Current Player:{turn}</h2>
             <main className="board">
-            <Square num={0}/>
+            <Square num={0} className="tile"/>
             <Square num={1}/>
             <Square num={2}/>
             <Square num={3}/>
@@ -35,8 +43,13 @@ export default function PlayerDisplay() {
             <Square num={6}/>
             <Square num={7}/>
             <Square num={8}/>
-
             </main>
+
+        <div className="reset-button">
+            <button onClick={resetBoard}>PLAY AGAIN</button>
+        </div>
+
+        <Announcer cells={cells} />
            
         </div>
     )
